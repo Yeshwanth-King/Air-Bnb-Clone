9048833b43c4ea7831cb5b7cd614988a0409e7e6
YESHWANTH R <yeshuyeshwanth2005@gmail.com>
2024-11-11 13:21:41 +0530

    Delete and setting main photo done

--- app/places/[id]/page.jsx
+++ app/places/[id]/page.jsx
@@ -3,6 +3,7 @@ import Navbar from "@/app/components/Navbar";
 import React, { useState, useEffect } from "react";
 import { RiUploadCloud2Line } from "react-icons/ri";
 import Perks from "@/app/components/Perks";
+import { FaRegMinusSquare } from "react-icons/fa";
 import axios from "axios";
 import SpinnerLoader from "@/app/components/SpinnerLoader";
 import { useRouter } from "next/navigation";
@@ -120,6 +121,20 @@ const Page = ({ params }) => {
     }
   };
 
+  const removePhoto = (ev, link) => {
+    ev.preventDefault();
+    setAddPhotos((prev) => {
+      return prev.filter((item) => link !== item);
+    });
+  };
+
+  const setTofirst = (ev, link) => {
+    ev.preventDefault();
+    setAddPhotos((prev) => {
+      return [link, ...prev.filter((item) => link !== item)];
+    });
+  };
+
   return (
     <div>
       <Navbar />
@@ -184,13 +199,54 @@ const Page = ({ params }) => {
                 return (
                   <div
                     key={link}
-                    className="flex justify-center items-center rounded-lg overflow-hidden w-40 h-40" // Use TailwindCSS for fixed dimensions
+                    className="flex relative justify-center items-center rounded-lg overflow-hidden w-40 h-40" // Use TailwindCSS for fixed dimensions
                   >
                     <img
                       className="rounded-lg object-cover w-full h-full" // Ensure the image covers the container
                       src={"/uploads/" + link}
                       alt=""
                     />
+                    <button
+                      onClick={(ev) => removePhoto(ev, link)}
+                      className="absolute bg-red-500 cursor-pointer opacity-80 p-1 top-1 rounded-lg right-1"
+                    >
+                      <FaRegMinusSquare className=" text-white text-xl font-bold" />
+                    </button>
+                    <button
+                      onClick={(ev) => setTofirst(ev, link)}
+                      className="absolute bg-black cursor-pointer opacity-80 p-1 top-1 rounded-lg left-1"
+                    >
+                      {link == addPhotos[0] && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-6 text-yellow-400"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      )}
+                      {link !== addPhotos[0] && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="0"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-6 text-white text-xl font-bold"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                          />
+                        </svg>
+                      )}
+                    </button>
                   </div>
                 );
               })}
